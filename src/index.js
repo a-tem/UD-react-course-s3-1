d9813c219d95b76f2aeb88830cd38c7e983839ad
Artem Voloshyn <artem.voloshyn@kinesso.com>
2021-07-23 16:51:44 +0300

    added markup for single comment added faker package for generating fake data for comment content

--- src/index.js
+++ src/index.js
@@ -2,11 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+import faker from 'faker';
+
 const App = () => {
-  return <div className="flex flex-col">
-    <div>App component works</div>
-    <div>Finally!</div>
-  </div>
+  return (
+    <div className="flex flex-row w-1/3 mb-4">
+      <div className="mr-2 w-48">
+        <img className="rounded" alt="avatar" src={faker.image.avatar()} />
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-row justify-between">
+          <div className="font-bold">{faker.name.firstName()}</div>
+          <div className="text-gray-400">{faker.date.past().toLocaleDateString()}</div>
+        </div>
+        <div className="">{faker.lorem.sentences(3)}</div>
+      </div>
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
