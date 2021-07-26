b1a5e3b84676a80b9cd212001db85c23cf99c19c
Artem Voloshyn <artem.voloshyn@kinesso.com>
2021-07-26 11:58:32 +0300

    added CommentDetail component for rendering single component content added props for ability to render dynamic data for every single comment

---
+++ src/components/CommentDetail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const CommentDetail = (props) => {
+  return (
+    <div className="flex flex-row w-1/3 mb-4">
+      <div className="mr-2 w-48">
+        <img className="rounded" alt="avatar" src={props.image} />
+      </div>
+      <div className="flex flex-col">
+        <div className="flex flex-row justify-between">
+          <div className="font-bold">{props.author}</div>
+          <div className="text-gray-400">{props.date}</div>
+        </div>
+        <div className="">{props.comment}</div>
+      </div>
+    </div>
+  );
+}
+
+export default CommentDetail;
--- src/index.js
+++ src/index.js
@@ -4,19 +4,26 @@ import './index.css';
 
 import faker from 'faker';
 
+import CommentDetail from './components/CommentDetail';
+
 const App = () => {
   return (
-    <div className="flex flex-row w-1/3 mb-4">
-      <div className="mr-2 w-48">
-        <img className="rounded" alt="avatar" src={faker.image.avatar()} />
-      </div>
-      <div className="flex flex-col">
-        <div className="flex flex-row justify-between">
-          <div className="font-bold">{faker.name.firstName()}</div>
-          <div className="text-gray-400">{faker.date.past().toLocaleDateString()}</div>
-        </div>
-        <div className="">{faker.lorem.sentences(3)}</div>
-      </div>
+    <div>
+      <CommentDetail author={faker.name.firstName()}
+                     date={faker.date.past().toLocaleDateString()}
+                     image={faker.image.avatar()}
+                     comment={faker.lorem.sentences(3)}
+      />
+      <CommentDetail author={faker.name.firstName()}
+                     date={faker.date.past().toLocaleDateString()}
+                     image={faker.image.avatar()}
+                     comment={faker.lorem.sentences(3)}
+      />
+      <CommentDetail author={faker.name.firstName()}
+                     date={faker.date.past().toLocaleDateString()}
+                     image={faker.image.avatar()}
+                     comment={faker.lorem.sentences(3)}
+      />
     </div>
   );
 }
